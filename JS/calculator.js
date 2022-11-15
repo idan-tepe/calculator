f22dfc8bfcf60f1b6c1f5be37646d06e5815911a
idan <idan.tepe15@gmail.com>
2022-11-15 18:08:53 +0200

    15.11

---
+++ JS/calculator.js
@@ -0,0 +1,28 @@
+const buttons = document.getElementsByTagName("button");
+let num1 = "";
+let num2 = "";
+let oper = "";
+
+for (let button = 0; button < buttons.length; button++) {
+  buttons[button].addEventListener("click", (e) => {
+    if (buttons[button].getAttribute("class") === "number") {
+      if (oper) {
+        num2 += buttons[button].getAttribute("id");
+        console.log(num2);
+      } else {
+        num1 += buttons[button].getAttribute("id");
+        console.log(num1);
+      }
+    } else if (buttons[button].getAttribute("class").includes("operator")) {
+      if (oper) {
+        oper = buttons[button].getAttribute("id");
+        console.log(num1 + oper + num2);
+        num1 = eval(num1 + oper + num2);
+        num2 = "";
+      } else {
+        oper = buttons[button].getAttribute("id");
+        console.log(oper);
+      }
+    }
+  });
+}
