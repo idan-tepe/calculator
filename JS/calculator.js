a1577990fcf7d3b397b6bde61ebef85f656218ee
idan <idan.tepe15@gmail.com>
2022-11-17 17:40:27 +0200

    '17.11'

--- JS/calculator.js
+++ JS/calculator.js
@@ -2,28 +2,53 @@ const buttons = document.getElementsByTagName("button");
 let num1 = "";
 let num2 = "";
 let oper = "";
-
+const display = document.getElementById("screen");
 for (let button = 0; button < buttons.length; button++) {
   buttons[button].addEventListener("click", (e) => {
-    if (buttons[button].getAttribute("class") === "number") {
-      if (oper) {
-        num2 += buttons[button].getAttribute("id");
-        console.log(num2);
-      } else {
-        num1 += buttons[button].getAttribute("id");
-        console.log(num1);
-      }
-    } else if (buttons[button].getAttribute("class").includes("operator")) {
-      if (oper) {
-        oper = buttons[button].getAttribute("id");
-        console.log(oper, num1, num2);
-        console.log(num1 + oper + num2);
-        num1 = eval(num1 + oper + num2);
-        num2 = "";
-      } else {
-        oper = buttons[button].getAttribute("id");
-        console.log(oper);
-      }
-    }
+    calcul(e.target);
   });
+}
+
+function calcul(button) {
+  if (button.getAttribute("class") === "number") {
+    if (oper) {
+      num2 += button.getAttribute("id");
+      display.innerHTML = num1 + oper + num2;
+    } else {
+      num1 += button.getAttribute("id");
+      display.innerHTML = num1;
+    }
+  } else if (button.getAttribute("class").includes("operator")) {
+    if (oper && num2) {
+      num1 = eval(num1 + oper + num2);
+      oper = button.getAttribute("id");
+      num2 = "";
+      display.innerHTML = num1 + oper;
+    } else {
+      oper = button.getAttribute("id");
+      display.innerHTML = num1 + oper;
+    }
+  } else if (button.getAttribute("id") === "c") {
+    display.innerHTML = "";
+    num1 = "";
+    num2 = "";
+    oper = "";
+  } else if (button.getAttribute("id") === "=") {
+    if (num1 && num2 && oper) {
+      display.innerHTML = eval(num1 + oper + num2);
+      num1 = eval(num1 + oper + num2);
+      oper = "";
+      num2 = "";
+    }
+  } else if (button.getAttribute("id") === ".") {
+    if (!oper && !num1.includes(".")) {
+      num1 = num1 + ".";
+      alert(1);
+      display.innerHTML = num1;
+    } else if (!num2.includes(".") && oper) {
+      num2 = num2 + ".";
+      alert(2);
+      display.innerHTML = num1 + oper + num2;
+    }
+  }
 }
